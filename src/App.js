3e657590a1a46425e080cd7f1df9b6d0e4ca14ef
ImAleexDz <alexndro7145@gmail.com>
2020-11-18 20:26:51 -0600

    example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import AuthApi from './AuthApi'
+import Cookies from 'js-cookie'
 
 function App() {
+
+  const [auth, setAuth] = React.useState(false);
+  const readCookie = () => {
+    const user = Cookies.get("user");
+    if(user) {
+      setAuth(true);
+    }
+  }
+
+  React.useEffect(() => {
+    readCookie();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthApi.Provider value={{auth, setAuth}}>
+        <Router>
+          <Routes />
+        </Router>
+      </AuthApi.Provider>
     </div>
   );
 }
+
+//Login
+const Login = () => {
+
+  const Auth = React.useContext(AuthApi);
+  const handleOnClick = () => {
+    Auth.setAuth(true);
+    Cookies.set("user", "loginTrue")
+  }
+    return (
+      <div>
+        <h1>Welcome to daily webcoding</h1>
+        <button onClick={handleOnClick}>Login</button>
+      </div>
+    )
+  }
+
+//Dashboard
+const Dashboard = () => {
+  const Auth = React.useContext(AuthApi);
+  const handleOnClick = () => {
+    Auth.setAuth(false);
+    Cookies.remove("user")
+  }
+    return (
+      <div>
+        <h1>Dashboard</h1>
+        <button onClick={handleOnClick}>Logout</button>
+      </div>
+    )
+  }
+
+const Routes = () => {
+  const Auth = React.useContext(AuthApi)
+  return(
+    <Switch>
+      <ProtectedLogin path="/login" component={Login} auth={Auth.auth}/>
+      <ProtectedDashboard path="/dashboard" auth={Auth.auth} component={Dashboard} />
+    </Switch>
+  )
+}
+
+//Protect Dashboard
+const ProtectedDashboard = ({auth, component:Component,...rest}) => (
+  <Route 
+    {...rest}
+    render={() => auth ?(
+      <Component />
+    ): 
+    
+    (
+      <Redirect to="/login" />
+    )}
+  />
+)
+
+const ProtectedLogin = ({auth, component:Component,...rest}) => (
+  <Route 
+    {...rest}
+    render={() => !auth ?(
+      <Component />
+    ): 
+    
+    (
+      <Redirect to="/dashboard" />
+    )}
+  />
+)
 
 export default App;
